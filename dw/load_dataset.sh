fc53bc259b373ecfbffcba6f2818b4fbbe7b3ce3
Andre Pereira <andrespp@gmail.com>
2018-07-26 14:30:18 -0300

    Use PDI from a docker image

--- dw/load_dataset.sh
+++ dw/load_dataset.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 #
 #
-#PDI_VERSION="ce-5.0.1.A-stable"
-PDI_VERSION="ce-7.1.0.0-12"
-
-PDI_KITCHEN="/home/andre/Downloads/pentaho/data-integration-$PDI_VERSION/kitchen.sh"
-PDI_PAN="/home/andre/Downloads/pentaho/data-integration-$PDI_VERSION/pan.sh"
+PDI_KITCHEN="docker container run --name etlprevds --rm -v $PWD:/jobs -v $PWD/kettle.properties:/data-integration/kettle.properties andrespp/pdi runj"
+PDI_PAN="docker container run --name etlprevds --rm -v $PWD:/jobs -v $PWD/kettle.properties:/data-integration/kettle.properties andrespp/pdi runt"
 
 A=`date`
 
@@ -17,41 +14,30 @@ PDI_TRF=""
 PDI_TRF="$PDI_TRF ./aux_create_tables.ktr"
 
 ## Dimension Tables
-#PDI_TRF="$PDI_TRF ./dim_data.ktr"
-#PDI_TRF="$PDI_TRF ./dim_situacao_beneficio.ktr"
-#PDI_TRF="$PDI_TRF ./dim_mot_cessacao.ktr"
-#PDI_TRF="$PDI_TRF ./dim_clientela.ktr"
-#PDI_TRF="$PDI_TRF ./dim_especie_classificacao.ktr"
-#PDI_TRF="$PDI_TRF ./dim_especie.ktr"
-#PDI_TRF="$PDI_TRF ./dim_sexo.ktr"
+PDI_TRF="$PDI_TRF ./dim_data.ktr"
+PDI_TRF="$PDI_TRF ./dim_situacao_beneficio.ktr"
+PDI_TRF="$PDI_TRF ./dim_mot_cessacao.ktr"
+PDI_TRF="$PDI_TRF ./dim_clientela.ktr"
+PDI_TRF="$PDI_TRF ./dim_especie_classificacao.ktr"
+PDI_TRF="$PDI_TRF ./dim_especie.ktr"
+PDI_TRF="$PDI_TRF ./dim_sexo.ktr"
 
 ## Fact Tables (core)
 PDI_TRF="$PDI_TRF ./fato_auxilio.ktr"
-#PDI_TRF="$PDI_TRF ./fato_aposentadoria.ktr"
-#PDI_TRF="$PDI_TRF ./fato_pensao2.ktr"
-#PDI_TRF="$PDI_TRF ./fato_pensao.ktr"
+PDI_TRF="$PDI_TRF ./fato_aposentadoria.ktr"
+PDI_TRF="$PDI_TRF ./fato_pensao2.ktr"
+PDI_TRF="$PDI_TRF ./fato_pensao.ktr"
 
 ## Fact Tables (auxiliary)
-#PDI_TRF="$PDI_TRF ./fato_auxilio_sample.ktr"
-#PDI_TRF="$PDI_TRF ./fato_auxilio_2015.ktr"
-#PDI_TRF="$PDI_TRF ./fato_auxilio_raw.ktr"
+PDI_TRF="$PDI_TRF ./fato_auxilio_sample.ktr"
+PDI_TRF="$PDI_TRF ./fato_auxilio_2015.ktr"
+PDI_TRF="$PDI_TRF ./fato_auxilio_raw.ktr"
 
 # Run transformations
 for i in $PDI_TRF ; do
-	echo $PDI_PAN -file $i
-	$PDI_PAN -file $i
+	echo $PDI_PAN $i
+	$PDI_PAN $i
 done
-
-#############################################
-## Full Job for Fact Tables (not recomended)
-PDI_JOB="./load_dataset.kjb"
-PDI_JOB_DIM="./load_dimensions.kjb"
-
-#echo $PDI_KITCHEN -file $PDI_JOB_DIM
-#$PDI_KITCHEN -file $PDI_JOB_DIM
-
-#echo $PDI_KITCHEN -file $PDI_JOB
-# $PDI_KITCHEN -file $PDI_JOB
 
 echo "Started at: $A"
 echo "Finised at: `date`"
